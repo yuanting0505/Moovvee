cf9de9389995c0a1544947b4032d81989fc61a77
yuanting0505 <yuantingisme@gmail.com>
2013-11-26 08:54:33 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GreenBean::Application.config.secret_key_base = '7dea561aaadd0d5b877ea6401cb01151a1a244536acf761571735f2edc410800d6322f490a31bfeb70dc9699f2103bb4641f66a7998a4936889469554b4bc222'
